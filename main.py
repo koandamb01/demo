ea159e1929c0011c1dd1fa1e066bd7e08b6c5741
Mohamed Koanda <m.koanda@aol.com>
2019-03-12 22:21:50 -0700

    new updates

--- main.py
+++ main.py
@@ -28,5 +28,5 @@ def fruits():
 # if __name__=="__main__":   
 #     app.run(debug=True)    
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=true)
+# if __name__ == '__main__':
+#     app.run(host='127.0.0.1', port=8080, debug=true)
